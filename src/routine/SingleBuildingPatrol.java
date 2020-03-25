2015fb0a7313680d6bcb3a7ff969c76b9108f12f
msalaman <Marcossalamanca97@hotmail.com>
2020-03-24 22:53:23 -0500

    added patrol

--- src/routine/SingleBuildingPatrol.java
+++ src/routine/SingleBuildingPatrol.java
@@ -1,8 +1,13 @@
 package routine;
 
+import java.util.Enumeration;
+import java.util.List;
+
 import blackboard.Blackboard;
 import bwapi.Game;
+import bwapi.Position;
 import bwapi.Race;
+import bwapi.Unit;
 
 public class SingleBuildingPatrol extends Routine {
 	@Override
@@ -11,7 +16,20 @@ public class SingleBuildingPatrol extends Routine {
 	 */
 	public int act(Game game, Blackboard blackboard) {
 		game.drawTextScreen(100, 200, "Patrol last building");
-		//TODO: Send idle troops to patrol the last building
+		// Send idle troops to patrol the last building
+		List<Unit> buildings = blackboard.getBuildings();
+		if(!buildings.isEmpty()) {
+	        Enumeration<String> e = blackboard.army.keys();
+	        Position pos = buildings.get(0).getPosition();
+	        while(e.hasMoreElements()) {
+	            String k = e.nextElement();
+	            List<Unit> troops = blackboard.army.get(k);
+	        	for (Unit troop : troops) {
+	        		if(troop.isIdle())
+	        		troop.move(pos);
+	    		}
+	        }
+		}
 		return -1;
 	}
 }
